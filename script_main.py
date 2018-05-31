a02836f0bbe1db340be3266d63b58a032ab46b0f
PayamZandiyeh <p.zandiyeh@gmail.com>
2018-05-31 10:05:51 -0500

    First scripts

---
+++ script_main.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 30 11:23:54 2018
+
+@author: Payam
+
+This script will order all the DICOM images in <input_dicom_directory>
+    into an image stack and write the data out as another image type as
+    specified by the extension of <output_file_name>.
+
+    If the flag <force> is set, the script does not check if the output exists.
+    Instead, it overwrites the data if it is there.
+
+    If the flag <verbose> is set, the script will print every DICOM file it finds.
+
+    Typically, this can be used for converting to .nii.gz.
+
+    This script is largely based off the ITK example Read DICOM Series and Write 3D Image 
+    """
+#%% import the libraries
+
+import itk
+from StereoFlouroscopyRegistration.io.read_image import get_itk_image_type
+import dicom_functions as dfun
+import numpy as np
+#%% inputs
+input_dicom_mask_directory = "/Volumes/Tashman Lab/SegmentedVolume" # the name of directory to read the volume from
+input_dicom_image_directory = "/Volumes/Tashman Lab/OriginalVolume" # the name of directory to read the volume from
+
+output_file_name = "/Volumes/Storage/Payam/Desktop/seg_vol.nii" # The output image location.
+force = True
+verbose = True
+#%% reading the dicom image
+mask  = dfun.dicom_reader(input_dicom_directory=input_dicom_mask_directory,verbose=verbose) # get the image pointer
+image = dfun.dicom_reader(input_dicom_directory=input_dicom_image_directory,verbose=verbose) # get the image pointer
+#%% printing the information of the image and mask
+if verbose:
+    print("Mask Information: \n") 
+    print(mask)
+    print("\n\n\n\n\n\n\n")
+    print("Original Volume Information: \n")
+    print(image)
+    
+#%% converting the mask to binary
+InputImageType = get_itk_image_type(mask.GetFileNames()[0]) # Getting the pixel type
+OutputImageType = get_itk_image_type(mask.GetFileNames()[0]) # Setting the output pixel type
+
+BinaryThresholdFilterType = itk.BinaryThresholdImageFilter[InputImageType,OutputImageType]
+bwfilter = BinaryThresholdFilterType.New() # Setting the binary filter.
+bwfilter.SetInput(mask.GetOutput()) # Setting the inputs of the filter.
+bwfilter.SetOutsideValue(0) # setting the value for the outside the range
+bwfilter.SetInsideValue(1)
+bwfilter.SetLowerThreshold(700)
+bwfilter.SetUpperThreshold(800)
+bwfilter.Update()
+#%% convolve the mask to an image
+buff = itk.GetArrayFromImage(bwfilter.GetOutput()) # Casting the image pixels to a numpy array
+bwfilter_array = buff.copy() # getting the image into a matrix format. 
+
+buff = itk.GetArrayFromImage(image.GetOutput())
+image_array = buff.copy() # getting a copy of the image into a matrix format. 
+
+masked_image_array = np.multiply(image_array,bwfilter_array) # multiply the image with the mask
+#%%
+data = image_array[np.nonzero(bwfilter_array)] # select the non-zero elements of the 
+
+output_mean,output_ci_low,output_ci_up = dfun.mean_confidence_interval(data)
+output_std  = np.std(data )
+output_min  = np.min(data )
+output_max  = np.max(data )
+
+output = [output_mean,output_std,output_min,output_max,output_ci_low,output_ci_up] # confidence interval doesn't make sense
+
+image2 = itk.GetImageFromArray(masked_image_array)
+#%% Write out
+final_image = image2# the image to be written
+
+
+print('Writing to {}'.format(output_file_name))
+itk.imwrite(final_image, str(output_file_name))
+print('finished')
